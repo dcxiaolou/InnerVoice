a0a376d2d0b1acf101f11f6e63db8953403a41d3
dcxiaolou <3515668588@qq.com>
2018-10-30 14:56:39 +0800

    依赖库修改，排除bmob-sdk自带的okhttp、glide等外部依赖库，便于导入最新的这些库，也更易于使用

---
+++ app/src/main/java/com/android/dcxiaolou/innervoice/adapter/FragmentAdapter.java
@@ -0,0 +1,32 @@
+package com.android.dcxiaolou.innervoice.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+* 主页、树洞页、信息页、个人中心页的适配器
+* */
+
+public class FragmentAdapter extends FragmentPagerAdapter {
+
+    private List<Fragment> mFragments = new ArrayList<>();
+
+    public FragmentAdapter(FragmentManager fm, List<Fragment> fragments) {
+        super(fm);
+        this.mFragments = fragments;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return mFragments.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragments.size();
+    }
+}
---
+++ app/src/main/java/com/android/dcxiaolou/innervoice/fragemnt/MessageFragment.java
@@ -0,0 +1,27 @@
+package com.android.dcxiaolou.innervoice.fragemnt;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.dcxiaolou.innervoice.R;
+
+/*
+* 信息页
+* */
+
+public class MessageFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_message, null);
+
+        return view;
+    }
+}
