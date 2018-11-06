5ad58eccd049f13b1453819921f07680456b851b
dcxiaolou <3515668588@qq.com>
2018-11-06 15:21:42 +0800

    欢迎页添加倒计时进度条（CountdownProgressBar）,主页每日精选模块添加文章详细内容展示（即阅读页面）

--- app/src/main/java/com/android/dcxiaolou/innervoice/ShowArticleAndCommon.java
+++ app/src/main/java/com/android/dcxiaolou/innervoice/ShowArticleAndCommon.java
@@ -1,11 +1,13 @@
 package com.android.dcxiaolou.innervoice;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.TextView;
 
 import com.android.dcxiaolou.innervoice.mode.ReadArticleResult;
+
+import org.sufficientlysecure.htmltextview.HtmlHttpImageGetter;
+import org.sufficientlysecure.htmltextview.HtmlTextView;
 
 public class ShowArticleAndCommon extends AppCompatActivity {
 
@@ -14,12 +16,49 @@ public class ShowArticleAndCommon extends AppCompatActivity {
 
     private ReadArticleResult articleResult;
 
+    private TextView titleTv, pushTimeTv, readNumTv, likeNumTv, commonNumTv;
+
+    private String title, pushTime, readNum, likeNum, commonNum, context;
+
+    private HtmlTextView articleContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_article_and_common);
 
-        articleResult = (ReadArticleResult) getIntent().getSerializableExtra(ARTICLE_DETAIL);
-        Log.d(TAG, "title = " + articleResult.getTitle());
+        //初始化界面
+        initView();
+
+        //初始化数据
+        initData();
     }
+
+    private void initView() {
+        titleTv = (TextView) findViewById(R.id.article_title);
+        pushTimeTv = (TextView) findViewById(R.id.article_push_time);
+        readNumTv = (TextView) findViewById(R.id.article_read_num);
+        likeNumTv = (TextView) findViewById(R.id.article_like_num);
+        commonNumTv = (TextView) findViewById(R.id.article_common_num);
+
+        articleContext = (HtmlTextView) findViewById(R.id.article_context);
+
+    }
+
+    private void initData() {
+        articleResult = (ReadArticleResult) getIntent().getSerializableExtra(ARTICLE_DETAIL);
+        //Log.d(TAG, "title = " + articleResult.getTitle());
+        title = articleResult.getTitle();
+        pushTime = articleResult.getPush_time();
+        readNum = articleResult.getCount();
+        likeNum = articleResult.getLike().get(0);
+        context = articleResult.getArticle_detail().get(0);
+
+        titleTv.setText(title);
+        pushTimeTv.setText(pushTime);
+        readNumTv.setText(readNum);
+        likeNumTv.setText(likeNum);
+        articleContext.setHtml(context, new HtmlHttpImageGetter(articleContext));
+    }
+
 }
--- app/src/main/java/com/android/dcxiaolou/innervoice/WelcomeActivity.java
+++ app/src/main/java/com/android/dcxiaolou/innervoice/WelcomeActivity.java
@@ -7,6 +7,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.dcxiaolou.innervoice.defineUI.CountDownProgressBar;
 
 /*
 * 欢迎页，便于主界面加载资源，避免出现黑屏
@@ -17,17 +20,29 @@ public class WelcomeActivity extends Activity {
 
     private ImageView mWelcomeIv;
 
+    // 倒计时进度条
+    private CountDownProgressBar cpb_countdown;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         mWelcomeIv = (ImageView) findViewById(R.id.welcome_iv);
 
+        cpb_countdown = (CountDownProgressBar) findViewById(R.id.cpb_countdown);
+
+        cpb_countdown.setDuration(3000, new CountDownProgressBar.OnFinishListener() {
+            @Override
+            public void onFinish() {
+
+            }
+        });
+
         //停顿5秒，便于主界面加载资源，避免出现黑屏
         //实现方法有Thread.sleep() handler 动画(AlphaAnimation ObjectAnimator)
         //mWelcomeIv 在哪里实现 alpha 动画效果透明 0.7f, 1.0f 透明度变化值
         ObjectAnimator animator = ObjectAnimator.ofFloat(mWelcomeIv, "alpha", 0.7f, 1.0f);
-        animator.setDuration(2000); //动画执行时间
+        animator.setDuration(3000); //动画执行时间
         animator.start();
         //给动画添加监听器 在动画结束的时候跳转到主界面
         animator.addListener(new AnimatorListenerAdapter() {
