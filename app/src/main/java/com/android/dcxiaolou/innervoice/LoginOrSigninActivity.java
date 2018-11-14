117a28b2bf4c42c60e15fb248104ddd23037350b
dcxiaolou <3515668588@qq.com>
2018-11-14 10:53:32 +0800

    登录、注册界面的添加

---
+++ app/src/main/java/com/android/dcxiaolou/innervoice/LoginOrSigninActivity.java
@@ -0,0 +1,13 @@
+package com.android.dcxiaolou.innervoice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginOrSigninActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_or_signin);
+    }
+}
--- app/src/main/java/com/android/dcxiaolou/innervoice/fragemnt/CenterFragment.java
+++ app/src/main/java/com/android/dcxiaolou/innervoice/fragemnt/CenterFragment.java
@@ -1,5 +1,7 @@
 package com.android.dcxiaolou.innervoice.fragemnt;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,21 +9,50 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.android.dcxiaolou.innervoice.LoginOrSigninActivity;
 import com.android.dcxiaolou.innervoice.R;
 
 /*
 * 个人中心页
 * */
 
-public class CenterFragment extends Fragment {
+public class CenterFragment extends Fragment implements View.OnClickListener {
+
+
+
+    private View mRootView;
+    private Context mContext;
+
+    private TextView logInOrSignIn;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_center, null);
+        mRootView = inflater.inflate(R.layout.fragment_center, null);
+        mContext = getContext();
+        return mRootView;
+    }
 
-        return view;
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        logInOrSignIn = (TextView) mRootView.findViewById(R.id.log_in_or_sign_in);
+
+        logInOrSignIn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.log_in_or_sign_in:
+                Intent loginIntent = new Intent(mContext, LoginOrSigninActivity.class);
+                mContext.startActivity(loginIntent);
+                break;
+        }
     }
 }
